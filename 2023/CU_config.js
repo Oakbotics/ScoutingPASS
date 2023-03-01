588e1e6a4888d8da0694489990c9961b98e241f6
DoodleLord <sasha.truscott@gmail.com>
2023-02-28 19:41:38 -0500

    proto2

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2019onlon",
+      "defaultValue": "2020ilch",
       "required": "true",
       "disabled": "true"
     },
